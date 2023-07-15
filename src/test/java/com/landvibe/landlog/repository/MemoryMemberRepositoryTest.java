eba76cd652b8722d7bbffbd6027aff98efecb289
박준영 <wnsdud6969@naver.com>
2023-07-16 01:40:16 +0900

    test(MemoryMemberRepository): test method findByEmailWithPassword

--- src/test/java/com/landvibe/landlog/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/com/landvibe/landlog/repository/MemoryMemberRepositoryTest.java
@@ -1,67 +1,86 @@
 package com.landvibe.landlog.repository;
 
 import com.landvibe.landlog.domain.Member;
+
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MemoryMemberRepositoryTest {
 
-    MemoryMemberRepository repository = new MemoryMemberRepository();
+	MemoryMemberRepository repository = new MemoryMemberRepository();
 
-    @AfterEach
-    public void afterEach() {
-        repository.clearStore();
-    }
+	@AfterEach
+	public void afterEach() {
+		repository.clearStore();
+	}
 
-    @Test
-    void save() {
-        //given
-        Member member = new Member();
-        member.setName("spring");
+	@Test
+	void save() {
+		//given
+		Member member = new Member();
+		member.setEmail("spring");
 
-        //when
-        repository.save(member);
+		//when
+		repository.save(member);
 
-        //then
-        Member result = repository.findById(member.getId()).get();
-        assertThat(result).isEqualTo(member);
-    }
+		//then
+		Member result = repository.findById(member.getId()).get();
+		assertThat(result).isEqualTo(member);
+	}
 
-    @Test
-    public void findByName() {
-        //given
-        Member member1 = new Member();
-        member1.setName("spring1");
-        repository.save(member1);
-        Member member2 = new Member();
-        member2.setName("spring2");
-        repository.save(member2);
+	@Test
+	public void findByName() {
+		//given
+		Member member1 = new Member();
+		member1.setEmail("spring1");
+		repository.save(member1);
+		Member member2 = new Member();
+		member2.setEmail("spring2");
+		repository.save(member2);
 
-        //when
-        Member result = repository.findByName("spring1").get();
+		//when
+		Member result = repository.findByName("spring1").get();
 
-        //then
-        assertThat(result).isEqualTo(member1);
-    }
+		//then
+		assertThat(result).isEqualTo(member1);
+	}
 
-    @Test
-    public void findAll() {
-        //given
-        Member member1 = new Member();
-        member1.setName("spring1");
-        repository.save(member1);
-        Member member2 = new Member();
-        member2.setName("spring2");
-        repository.save(member2);
+	@Test
+	public void findAll() {
+		//given
+		Member member1 = new Member();
+		member1.setEmail("spring1");
+		repository.save(member1);
+		Member member2 = new Member();
+		member2.setEmail("spring2");
+		repository.save(member2);
 
-        //when
-        List<Member> result = repository.findAll();
+		//when
+		List<Member> result = repository.findAll();
 
-        //then
-        assertThat(result.size()).isEqualTo(2);
-    }
+		//then
+		assertThat(result.size()).isEqualTo(2);
+	}
+
+	@Test
+	@DisplayName("이메일과 비밀번호가 일치하는 Member 찾는 테스트")
+	public void findByEmailWithPassword() {
+		Member member = new Member();
+		member.setEmail("123");
+		member.setPassword("456");
+
+		repository.save(member);
+
+		Optional<Member> successFindMember = repository.findByEmailWithPassword("123", "456");
+		Optional<Member> failFindMember = repository.findByEmailWithPassword("123", "wrong password");
+
+		assertThat(successFindMember.isEmpty()).isNotEqualTo(null);
+		assertThat(failFindMember.isEmpty()).isEqualTo(true);
+	}
 }
