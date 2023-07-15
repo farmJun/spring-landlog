cea937e048e8ffed9f3266f7684658d0b35f8475
박준영 <wnsdud6969@naver.com>
2023-07-16 01:19:14 +0900

    refactor(Member): change name to email

--- src/main/java/com/landvibe/landlog/domain/Member.java
+++ src/main/java/com/landvibe/landlog/domain/Member.java
@@ -2,30 +2,31 @@ package com.landvibe.landlog.domain;
 
 public class Member {
 
-    private Long id;
-    private String name;
+	private Long id;
+	private String email;
 
-    public Member() {
-    }
+	public Member() {
+	}
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+	public Member(Long id, String email) {
+		this.id = id;
+		this.email = email;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
