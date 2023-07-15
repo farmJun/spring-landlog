06d113e02ec53ba69e68279652bb2297058c2290
박준영 <wnsdud6969@naver.com>
2023-07-16 01:26:33 +0900

    feat(MemberForm): add password

--- src/main/java/com/landvibe/landlog/controller/MemberForm.java
+++ src/main/java/com/landvibe/landlog/controller/MemberForm.java
@@ -1,13 +1,23 @@
 package com.landvibe.landlog.controller;
 
 public class MemberForm {
-    private String name;
+	private String email;
 
-    public String getName() {
-        return name;
-    }
+	private String password;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
