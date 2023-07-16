80fb710c29aba422ec68397be2e1355f39b403c0
박준영 <wnsdud6969@naver.com>
2023-07-16 16:04:32 +0900

    feat(Member): add name

--- src/main/java/com/landvibe/landlog/domain/Member.java
+++ src/main/java/com/landvibe/landlog/domain/Member.java
@@ -2,39 +2,48 @@ package com.landvibe.landlog.domain;
 
 public class Member {
 
-	private Long id;
-	private String email;
-	private String password;
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
 
-	public Member() {
-	}
+    public Member() {
+    }
 
-	public Member(Long id, String email) {
-		this.id = id;
-		this.email = email;
-	}
+    public Member(Long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
