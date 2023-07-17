d8e1510a3a3f2034cad89385e9684838150d13fe
박준영 <wnsdud6969@naver.com>
2023-07-18 00:31:53 +0900

    style(MemberRepository): remove comment

--- src/main/java/com/landvibe/landlog/repository/MemberRepository.java
+++ src/main/java/com/landvibe/landlog/repository/MemberRepository.java
@@ -7,9 +7,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-
-    //변경 전 입니다.
-    // 변경 후 입니다.
     Member save(Member member);
 
     Optional<Member> findById(Long id);
