55e000820c92c16559ffe95d2b15e93aa5bbd2b5
박준영 <wnsdud6969@naver.com>
2023-07-23 17:40:32 +0900

    feat(Member): add constructor

--- src/main/java/com/landvibe/landlog/domain/Member.java
+++ src/main/java/com/landvibe/landlog/domain/Member.java
@@ -15,6 +15,13 @@ public class Member {
         this.email = email;
     }
 
+    public Member(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
     public Long getId() {
         return id;
     }
